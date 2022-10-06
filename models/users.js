44ccc7de6327f465ce4a27fd814fed2aa3618ea7
mig <michaelleojacob@gmail.com>
2022-10-06 12:16:04 -0700

    +username minlength down from 6 to 1

--- models/users.js
+++ models/users.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const users = new Schema({
-  username: { type: String, required: true, minLength: 3 },
+  username: { type: String, required: true, minLength: 1 },
   password: { type: String, required: true, minLength: 6 },
 });
 
