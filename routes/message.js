9eeafa23a70f80832b911620302addd64dd4f2ef
mig <michaelleojacob@gmail.com>
2022-10-08 18:57:23 -0700

    +if else in devnav.pug to display user if logged in. +minor changes to message-form.pug

--- routes/message.js
+++ routes/message.js
@@ -2,7 +2,7 @@ const express = require('express');
 const messageRouter = express.Router();
 
 messageRouter.get('/', (req, res, next) => {
-  res.render('message-form');
+  res.render('message-form', { user: req.user });
 });
 
 module.exports = messageRouter;
