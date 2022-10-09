9e65020435bd696a101ce06e1f4f24f57ff89409
mig <michaelleojacob@gmail.com>
2022-10-09 14:41:30 -0700

    +logic for user /roles/auth in routes/homepage.js +view depending on auth in /views/home.pug

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const homepageRouter = express.Router();
+const Messages = require('../models/messages.js');
+
+const getHighestAuth = (user) => {
+  let highestAuth = 'basic';
+  if (user.roles.includes('member')) highestAuth = 'member';
+  if (user.roles.includes('admin')) highestAuth = 'admin';
+  return highestAuth;
+};
 
 homepageRouter.get('/', (req, res, next) => {
   // for testing, going to keep it /login
   // if (!currentUser) return res.redirect('/register');
   if (!currentUser) return res.redirect('/login');
-  return res.render('home', { user: currentUser });
+
+  const auth = getHighestAuth(currentUser);
+
+  // get all messages
+  Messages.find({})
+    .sort({ timeStamp: 'descending' })
+    .exec((err, msgs) => {
+      if (err) return next(err);
+      return res.render('home', {
+        user: currentUser,
+        msg_list: msgs,
+        auth: auth,
+      });
+    });
 });
 
 module.exports = homepageRouter;
